ec2e3d8172ef982fc645773fe518532f9b02ebeb
fernando-bg <82680852+fernando-bg@users.noreply.github.com>
2021-09-20 10:22:57 -0500

    Update datos_muni.py

--- datos_muni.py
+++ datos_muni.py
@@ -7,24 +7,18 @@ Created on Tue May 11 22:58:16 2021
 """
 
 
-proyecto = 'Mejoramiento de los Servicios de Comercialización del Mercado Modelo La Parada, Distrito de Pariñas, Provincia de Talara, Departamento de Piura'
+proyecto = 'Creación de los Servicios de Comercialización en el Gran Mercado Modelo de la ciudad de Chota, distrito de Chota, Chota - Cajamarca'
 
 # Encuestadores
 
 lista_encuestadores =  [
-    {'label':'KARINA ROXANA SILVA MERINO', 'value':'43070962'},
-    {'label':'CAROLINA MARTINEZ CALDERON', 'value':'42181054'},
-    {'label':'TIFFANI LITANO CABANILLAS', 'value':'72919131'},
-    {'label':'DANDER HENRY MARTINEZ CALDERON', 'value':'43396264'},
-    {'label':'SOFIA CHIRINOS SIANCAS', 'value':'72121913'},
-    {'label':'VANESSA YACKELINE ESPINOZA RETO', 'value':'40433028'},
-    {'label':'ROSA ELVIRA COVEÑAS FRANCO', 'value':'44347157'},
-    {'label':'MIRTHA ZAVALA ESPINOZA', 'value':'72650688'},
-    {'label':'LEYDY ESTEFANY SILVA MORALES', 'value':'76436965'},
-    {'label':'MARIA ELIZABETH SILVA MERINO', 'value':'45300361'}
+    {'label':'DIANA LIZETH GUEVARA VASQUEZ', 'value':'76790863'},
+    {'label':'MARIA BELLIA COLUNCHE VÁZQUEZ', 'value':'74326354'},
+    {'label':'HERLY ANALI GARCÍA CUBAS', 'value':'71266698'},
+    {'label':'DORIS SARELA NUÑEZ COLUNCHE', 'value':'99999999'}
     ]
 
 
 lista_supervisores =  [
-    {'label':'JULIANA SILVA MERINO', 'value':'42227572'}
+    {'label':'MARIA BELLIA COLUNCHE VÁZQUEZ', 'value':'74326354'}
     ]
