cd0863e74cba8d936656a819aa07e576580f6a6b
fernando-bg <82680852+fernando-bg@users.noreply.github.com>
2021-10-17 21:55:26 -0500

    Update datos_muni.py

--- datos_muni.py
+++ datos_muni.py
@@ -7,18 +7,22 @@ Created on Tue May 11 22:58:16 2021
 """
 
 
-proyecto = 'Creación de los Servicios de Comercialización en el Gran Mercado Modelo de la ciudad de Chota, distrito de Chota, Chota - Cajamarca'
+proyecto = 'Mejoramiento y Ampliación de Servicio de Accesibilidad y Comercialización de Productos de 1era Necesidad del Mercado de Abastos Rosaspata - Cusco'
 
 # Encuestadores
 
 lista_encuestadores =  [
-    {'label':'DIANA LIZETH GUEVARA VASQUEZ', 'value':'76790863'},
-    {'label':'MARIA BELLIA COLUNCHE VÁZQUEZ', 'value':'74326354'},
-    {'label':'HERLY ANALI GARCÍA CUBAS', 'value':'71266698'},
-    {'label':'DORIS SARELA NUÑEZ COLUNCHE', 'value':'99999999'}
+    {'label':'CAROLINE VALLEJOS CHÁVEZ', 'value':'45199105'},
+    {'label':'AYLING MARITSA ALVAREZ VILLEGAS', 'value':'71632260'},
+    {'label':'JORGE HUAMANTALLA ESPEJO', 'value':'70415624'},
+    {'label':'GIUSSEPPE DEVIN VALENCIA OCHOA', 'value':'72134427'},
+    {'label':'VALERIA ROSARIO DELGADO CHÁVEZ', 'value':'70671212'},
+    {'label':'JORGE ALBERTO GASPAR MENDOZA', 'value':'23914643'},
+    {'label':'SOFIA ALARCON HUILLCA', 'value':'48709519'},
+    {'label':'SANDRA PAOLA MIRANDA CEVALLOS', 'value':'46285049'}
     ]
 
 
 lista_supervisores =  [
-    {'label':'MARIA BELLIA COLUNCHE VÁZQUEZ', 'value':'74326354'}
+    {'label':'CAROLINE VALLEJOS CHÁVEZ', 'value':'45199105'}
     ]
