ca30dd20a7b08ea0ab9f8cf1deac10edad991962
fernando-bg <82680852+fernando-bg@users.noreply.github.com>
2021-11-22 09:53:11 -0500

    Update datos_muni.py

--- datos_muni.py
+++ datos_muni.py
@@ -7,34 +7,22 @@ Created on Tue May 11 22:58:16 2021
 """
 
 
-proyecto = 'Mejoramiento y Ampliación de Servicio de Accesibilidad y Comercialización de Productos de 1era Necesidad del Mercado de Abastos Rosaspata - Cusco'
+proyecto = 'Mejoramiento y Ampliación del Servicio de Comercialización del Mercado Central de Catacaos, Distrito de Catacaos, Provincia de Piura, Departamento de Piura'
 
 # Encuestadores
 
 lista_encuestadores =  [
-    {'label':'CAROLINE VALLEJOS CHÁVEZ', 'value':'45199105'},
-    {'label':'AYLING MARITSA ALVAREZ VILLEGAS', 'value':'71632260'},
-    {'label':'JORGE HUAMANTALLA ESPEJO', 'value':'70415624'},
-    {'label':'GIUSSEPPE DEVIN VALENCIA OCHOA', 'value':'72134427'},
-    {'label':'VALERIA ROSARIO DELGADO CHÁVEZ', 'value':'70671212'},
-    {'label':'JORGE ALBERTO GASPAR MENDOZA', 'value':'23914643'},
-    {'label':'SOFIA ALARCON HUILLCA', 'value':'48709519'},
-    {'label':'SANDRA PAOLA MIRANDA CEVALLOS', 'value':'46285049'},
-    {'label':'YANETH HURTADO MAÑACCASA', 'value':'41412904'},
-    {'label':'LAURA MAEVA VALVERDE FRANCO', 'value':'72800230'},
-    {'label':'JOSÉ HUMBERTO CABEZAS MANCILLA', 'value':'42216902'},
-    {'label':'AUDRIC GARCIA HOLGADO', 'value':'42563076'},
-    {'label':'VÍCTOR JOSEPH ARTEGA ESCOBAR', 'value':'44268524'},
-    {'label':'JORGE IVÁN GUTIÉRREZ LEZAMA', 'value':'40899251'},
-    {'label':'HUGO MARK ANTHONY FLORES SALAZAR', 'value':'74204776'},
-    {'label':'CHRISTIAAN MANUEL VALVERDE FRANCO', 'value':'44148661'},
-    {'label':'DANIEL ELIAS DELGADO CEVALLOS', 'value':'24007047'},
-    {'label':'GIANCARLO LUIS HERRERA DELGADO', 'value':'43925256'},
-    {'label':'CHRISTOPHER FERNÁNDEZ SÁNCHEZ', 'value':'73063798'},
-    {'label':'DANIEL MORMONTOY PEÑALBA', 'value':'72381408'}
+    {'label':'YAJAIRA VILLAFUERTE PAICO', 'value':'71244026'},
+    {'label':'FRÉDY JULIO MEJÍA ASCARATE', 'value':'48278977'},
+    {'label':'PABLO AGUSTIN ELIAS CHERO', 'value':'71992561'},
+    {'label':'MILAGROS LIZBETH AQUINO MORALES', 'value':'76075824'},
+    {'label':'LUCY JACKELINE VIGIL PERALTA', 'value':'76333455'},
+    {'label':'DANIEL FERNANDO MORE LABRIN', 'value':'73673785'},
+    {'label':'JAVIER ALEJANDRO DURAND BURNEO', 'value':'76629305'},
+    {'label':'LIZETH IRENE CUEVA GÓMEZ', 'value':'40636760'}
     ]
 
 
 lista_supervisores =  [
-    {'label':'CAROLINE VALLEJOS CHÁVEZ', 'value':'45199105'}
+    {'label':'LIZETH IRENE CUEVA GÓMEZ', 'value':'40636760'}
     ]
